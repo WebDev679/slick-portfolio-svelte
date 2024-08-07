00aa24e15c4346c186100e84a5db742db8afab57
Arnav Chaudhry <50321710+WebDev679@users.noreply.github.com>
2024-08-06 22:58:23 -0400

    updated info

--- src/lib/data/education.ts
+++ src/lib/data/education.ts
@@ -3,28 +3,16 @@ import type { Education } from '../types';
 
 export const items: Array<Education> = [
 	{
-		degree: 'Bachelor degree of Computer Science',
+		degree: 'Bachelors in Computer Science',
 		description: '',
-		location: 'Tunisia',
+		location: 'Boston, MA',
 		logo: Assets.Unknown,
 		name: '',
-		organization: 'ISTIC',
-		period: { from: new Date(2020, 0, 1), to: new Date(2022, 5, 1) },
+		organization: 'Boston University',
+		period: { from: new Date(2022, 9, 1) },
 		shortDescription: '',
 		slug: 'dummy-education-item',
-		subjects: ['C', 'Algorithm', 'Algebra', 'Python', 'C++', 'Java', 'English']
-	},
-	{
-		degree: 'PhD of Computer Science',
-		description: '',
-		location: 'USA',
-		logo: Assets.Unknown,
-		name: '',
-		organization: 'MIT',
-		period: { from: new Date(2023, 0, 1) },
-		shortDescription: '',
-		slug: 'dummy-education-item-2',
-		subjects: ['Assembly', 'Rust', 'Computer Architecture', 'Algorithms and Data structures']
+		subjects: ['Data Structures & Algorithms', 'Database Design & Implementaion', 'Discrete Mathematics', 'Probability & Statistics',' Logic Design', 'Mobile App Development', 'Object Oriented Programming', 'Computer Architecture', 'Foundations of Data Science'],
 	}
 ];
 
